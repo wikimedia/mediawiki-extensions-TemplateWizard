17d1575ed3a83192e5c92ca67707001bbd2708e2
Sam Wilson <sam@samwilson.id.au>
2018-10-03 09:45:02 +0800

    Add BetaFeatures support
    Bug: T205290
    Change-Id: I81f3ab6827fd0448ac3f9533a42761eb4ee9be0c

--- includes/Hooks.php
+++ includes/Hooks.php
@@ -6,13 +6,19 @@
 namespace MediaWiki\Extension\TemplateWizard;
 
 use EditPage;
+use ExtensionRegistry;
+use MediaWiki\MediaWikiServices;
 use OutputPage;
 use ResourceLoader;
+use User;
 
 /**
  * Hooks for the TemplateWizard extension.
  */
 class Hooks {
+
+	/** @var string The name of the beta-feature. */
+	private static $betaFeature = 'templatewizard-betafeature';
 
 	/**
 	 * Add the extension's module.
@@ -24,7 +30,14 @@ class Hooks {
 		EditPage $editPage,
 		OutputPage $output
 	) {
-		$output->addModules( 'ext.TemplateWizard' );
+		// Enable TemplateWizard if the BetaFeatures extension is not loaded,
+		// or if the user has enabled it as a beta-feature.
+		if (
+			!ExtensionRegistry::getInstance()->isLoaded( 'BetaFeatures' )
+			|| \BetaFeatures::isFeatureEnabled( $editPage->getContext()->getUser(), static::$betaFeature )
+		) {
+			$output->addModules( 'ext.TemplateWizard' );
+		}
 	}
 
 	/**
@@ -48,5 +61,28 @@ class Hooks {
 			]
 		];
 		return true;
+	}
+
+	/**
+	 * Add TemplateWizard as a beta feature.
+	 * @see https://www.mediawiki.org/wiki/Extension:BetaFeatures/GetBetaFeaturePreferences
+	 * @param User $user
+	 * @param mixed[] &$betaPrefs
+	 */
+	public static function onGetBetaFeaturePreferences( User $user, &$betaPrefs ) {
+		$extensionAssetsPath = MediaWikiServices::getInstance()
+			->getMainConfig()
+			->get( 'ExtensionAssetsPath' );
+		$mediawikiOrgBaseUrl = 'https://www.mediawiki.org/wiki/Special:MyLanguage/';
+		$betaPrefs[static::$betaFeature ] = [
+			'label-message' => 'templatewizard-betafeature-message',
+			'desc-message' => 'templatewizard-betafeature-description',
+			'screenshot' => [
+				'ltr' => "$extensionAssetsPath/TemplateWizard/images/betafeature-ltr.svg",
+				'rtl' => "$extensionAssetsPath/TemplateWizard/images/betafeature-rtl.svg",
+			],
+			'info-link' => $mediawikiOrgBaseUrl . 'Help:Extension:TemplateWizard',
+			'discussion-link' => $mediawikiOrgBaseUrl . 'Help_talk:Extension:TemplateWizard',
+		];
 	}
 }
