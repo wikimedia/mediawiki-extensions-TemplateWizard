559ac7a9c4cde7467cba3a5f7d6047725462ef0e
ephemeralwaves <thinkcolorful@thinkcolorful.org>
2020-04-26 20:00:17 -0400

    Selenium: add page object
    Add page object for creation of new template. Moved the functions in the
    before hook to the new page object. Also fixed the name for the third
    test to be more readable.
    
    Bug: T240766
    Change-Id: I55461561a80dbcf8f8b397a943496a6b29b9db95

---
+++ tests/selenium/pageobjects/CreateTemplate.page.js
@@ -0,0 +1,23 @@
+const Page = require( 'wdio-mediawiki/Page' ),
+	Api = require( 'wdio-mediawiki/Api' ),
+	fs = require( 'fs' ),
+	path = require( 'path' );
+
+class CreateTemplate extends Page {
+
+	get testTemplate() { return $( '.mw-templatedata-doc-wrap' ); }
+
+	create( testTemplateName ) {
+		browser.call( function () {
+			var templateWikitext = fs.readFileSync( `${path.dirname( __dirname )}/fixtures/en.Example.wikitext` );
+			return Api.edit( `Template:${testTemplateName}`, templateWikitext );
+		} );
+	}
+	open( testTemplateName ) {
+		super.openTitle( `Template:${testTemplateName}` );
+		this.testTemplate.waitForVisible();
+	}
+
+}
+
+module.exports = new CreateTemplate();
