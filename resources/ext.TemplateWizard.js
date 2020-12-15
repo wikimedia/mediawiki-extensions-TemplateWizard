ebd3920b8e53844366f1bce00e6789623f1ad2b7
Adam Wight <adam.wight@wikimedia.de>
2020-12-15 10:58:07 +0100

    New event semantics for performer fields
    This is consistent with newer schemas: we wrap in a "performer"
    object, and don't send user fields for anonymous users.  This is more
    meaningful than sending "null" or "0" as legacy schemas do.
    
    Also sends the bucketed edit count.  Note that our buckets are
    slightly different than the usual [0], [1-4], [5-99].  Instead, the
    low range is [0-10], [11-99].  Other intervals are the same.
    
    Bug: T262209
    Bug: T269986
    Depends-On: I8b35da81ab8548ec39d423bd843edbf8c97b51e8
    Change-Id: Id60b87a1811ede8e3e4757f282d7792dbb9efae2

--- resources/ext.TemplateWizard.js
+++ resources/ext.TemplateWizard.js
@@ -7,20 +7,44 @@ mw.TemplateWizard.insertedTemplates = [];
 /**
  * Convenience wrapper for mw.track(), to log an event with EventLogging.
  *
- * @param {string} event One of "launch", "remove-template", "use-recent", "insert-template",
+ * @param {string} action One of "launch", "remove-template", "use-recent", "insert-template",
  * "cancel-dialog", or "save-page".
  * @param {string[]} templateNames Array of template names (without 'Template:' prefix).
  */
-mw.TemplateWizard.logEvent = function ( event, templateNames ) {
-	mw.track( 'event.TemplateWizard', {
-		/* eslint-disable camelcase */
-		action: event,
-		namespace_id: mw.config.get( 'wgNamespaceNumber' ),
-		page_title: mw.config.get( 'wgTitle' ),
-		revision_id: mw.config.get( 'wgCurRevisionId' ),
-		user_id: mw.user.getId(),
-		user_edit_count: mw.config.get( 'wgUserEditCount', 0 ),
-		template_names: templateNames
-		/* eslint-enable camelcase */
-	} );
+mw.TemplateWizard.logEvent = function ( action, templateNames ) {
+	var editCount,
+		editCountBucket,
+		event = {
+			/* eslint-disable camelcase */
+			action: action,
+			namespace_id: mw.config.get( 'wgNamespaceNumber' ),
+			page_title: mw.config.get( 'wgTitle' ),
+			revision_id: mw.config.get( 'wgCurRevisionId' ),
+			template_names: templateNames
+			/* eslint-enable camelcase */
+		};
+	if ( !mw.user.isAnon() ) {
+		editCount = mw.config.get( 'wgUserEditCount' );
+		if ( editCount < 11 ) {
+			editCountBucket = 'under11';
+		} else if ( editCount < 100 ) {
+			editCountBucket = 'over10';
+		} else if ( editCount < 1000 ) {
+			editCountBucket = 'over100';
+		} else if ( editCount < 10000 ) {
+			editCountBucket = 'over1k';
+		} else {
+			editCountBucket = 'over10k';
+		}
+		$.extend( event, {
+			/* eslint-disable camelcase */
+			performer: {
+				user_id: mw.user.getId(),
+				user_edit_count: editCount,
+				user_edit_count_bucket: editCountBucket
+			}
+			/* eslint-enable camelcase */
+		} );
+	}
+	mw.track( 'event.TemplateWizard', event );
 };
