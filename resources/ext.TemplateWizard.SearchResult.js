f0acc7d2f9046d9255953f176ba3993df7cf3d81
Thiemo Kreuz <thiemo.kreuz@wikimedia.de>
2021-06-10 12:08:32 +0200

    Fix design issues related to template descriptions
    * An extremely long description was 100% shown, resulting in
      extremely large dropdown items. In VE this is limited to 1
      line only. To not change the existing design here entirely
      I suggest 2 lines. Some very recent CSS make this work in old
      and new browsers (possibly without ellipsis in old browsers,
      but that should be the only difference).
    * The font size in the dropdown is extremely small. The trivial
      fix here is to use the same font size as later when the
      description is shown in the header of the dialog.
    * The extremely narrow line height made the description very
      hard to read. I made it look very similar to how it looks in
      VisualEditor.
    * Long words made the header scroll horizontally. Now long
      words break instead.
    * I made sure the redirect never wraps on multiple lines, even
      if it's extremely long.
    
    Bug: T284890
    Change-Id: I6bfc515183d8d600c7ad09eb6f656482d34aceda

--- resources/ext.TemplateWizard.SearchResult.js
+++ resources/ext.TemplateWizard.SearchResult.js
@@ -6,23 +6,20 @@
  * @cfg {string} [data.redirecttitle] Page title for the "redirected from" message
  */
 mw.TemplateWizard.SearchResult = function MWTemplateWizardSearchResult( config ) {
-	var $description;
 	mw.TemplateWizard.SearchResult.super.call( this, config );
-
-	// Description.
-	$description = $( '<span>' )
-		.addClass( 'ext-templatewizard-description' )
-		.append( $( '<bdi>' ).text( config.description || '' ) );
 
 	if ( config.data.redirecttitle ) {
 		var redirecttitle = new mw.Title( config.data.redirecttitle ).getMainText();
 		$( '<div>' )
 			.addClass( 'ext-templatewizard-redirectedfrom' )
 			.text( mw.msg( 'redirectedfrom', redirecttitle ) )
-			.prependTo( $description );
+			.appendTo( this.$element );
 	}
 
-	this.$element.append( $description );
+	$( '<span>' )
+		.addClass( 'ext-templatewizard-description' )
+		.append( $( '<bdi>' ).text( config.description || '' ) )
+		.appendTo( this.$element );
 };
 
 OO.inheritClass( mw.TemplateWizard.SearchResult, OO.ui.MenuOptionWidget );
