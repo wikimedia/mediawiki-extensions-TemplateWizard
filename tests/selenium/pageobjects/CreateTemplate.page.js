365666bf05c350b51c913262673432fdadc53136
Peter Wangai <pwangai@wikimedia.org>
2023-03-14 18:22:13 +0300

    selenium: Refactor WebdriverIO tests from sync to async mode
    Refactor CreateTemplate page object to async.
    
    Bug: T300208
    Change-Id: I0e85ba687bb2af6140cd66a621ca8b3dec96315b

--- tests/selenium/pageobjects/CreateTemplate.page.js
+++ tests/selenium/pageobjects/CreateTemplate.page.js
@@ -9,16 +9,14 @@ class CreateTemplate extends Page {
 
 	get testTemplate() { return $( '.mw-templatedata-doc-wrap' ); }
 
-	create( testTemplateName ) {
-		browser.call( async () => {
-			const bot = await Api.bot();
-			const templateWikitext = fs.readFileSync( `${path.dirname( __dirname )}/fixtures/en.Example.wikitext` );
-			await bot.edit( `Template:${testTemplateName}`, templateWikitext );
-		} );
+	async create( testTemplateName ) {
+		const bot = await Api.bot();
+		const templateWikitext = fs.readFileSync( `${path.dirname( __dirname )}/fixtures/en.Example.wikitext` );
+		await bot.edit( `Template:${testTemplateName}`, templateWikitext );
 	}
-	open( testTemplateName ) {
-		super.openTitle( `Template:${testTemplateName}` );
-		this.testTemplate.waitForDisplayed();
+	async open( testTemplateName ) {
+		await super.openTitle( `Template:${testTemplateName}` );
+		await this.testTemplate.waitForDisplayed();
 	}
 
 }
