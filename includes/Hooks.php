73b669c452853eb3ace6882b551b32074f26aedb
Umherirrender <umherirrender_de.wp@web.de>
2023-09-17 12:00:21 +0200

    Use HookHandlers for core hooks
    Bug: T346565
    Change-Id: I293ee8e0b72eb4f4520c9d6f370f0ef6bfcf3b5b

--- includes/Hooks.php
+++ includes/Hooks.php
@@ -3,16 +3,24 @@
  * @file
  */
 
+// phpcs:disable MediaWiki.NamingConventions.LowerCamelFunctionsName.FunctionName
+
 namespace MediaWiki\Extension\TemplateWizard;
 
+use Config;
 use ExtensionRegistry;
 use MediaWiki\EditPage\EditPage;
+use MediaWiki\Hook\EditPage__showEditForm_initialHook;
+use MediaWiki\ResourceLoader\Hook\ResourceLoaderGetConfigVarsHook;
 use OutputPage;
 
 /**
  * Hooks for the TemplateWizard extension.
  */
-class Hooks {
+class Hooks implements
+	EditPage__showEditForm_initialHook,
+	ResourceLoaderGetConfigVarsHook
+{
 
 	/**
 	 * Add the extension's module.
@@ -20,9 +28,9 @@ class Hooks {
 	 * @param EditPage $editPage The current EditPage object.
 	 * @param OutputPage $output The OutputPage object.
 	 */
-	public static function onEditPageShowEditFormInitial(
-		EditPage $editPage,
-		OutputPage $output
+	public function onEditPage__showEditForm_initial(
+		$editPage,
+		$output
 	) {
 		$output->addModules( 'ext.TemplateWizard' );
 	}
@@ -31,8 +39,10 @@ class Hooks {
 	 * Adds extra variables to the global config
 	 *
 	 * @param array &$vars Global variables object
+	 * @param string $skin
+	 * @param Config $config
 	 */
-	public static function onResourceLoaderGetConfigVars( array &$vars ) {
+	public function onResourceLoaderGetConfigVars( array &$vars, $skin, Config $config ): void {
 		$extensionRegistry = ExtensionRegistry::getInstance();
 
 		$vars['wgTemplateWizardConfig'] = [
